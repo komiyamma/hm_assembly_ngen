06064202e83a7a0cacd2889632e30e8ff007ffdc
komiyamma <komiyama0629@gmail.com>
2022-04-22 16:40:41 +0900

    サブディレクトリもNGenターゲットとするようにした

--- HmAssemblyNGen.bat
+++ HmAssemblyNGen.bat
@@ -22,9 +22,8 @@ find hidemaru.exe "32bit edition"
 
 if %ERRORLEVEL%==0 (
 	echo "秀丸エディタ 32bit版";
-    for %%A in (%~dp0*.dll) do (
-        echo %%A
-        C:\Windows\Microsoft.NET\Framework\v4.0.30319\ngen.exe install %%A /AppBase:"%CURRENT_TMP_DIR_WITHOUT_LAST_SEPARATOR%"
+    for /R %targetdir% %%A in (*.dll) do (
+        C:\Windows\Microsoft.NET\Framework\v4.0.30319\ngen.exe install %%~fA /AppBase:"%CURRENT_TMP_DIR_WITHOUT_LAST_SEPARATOR%"
     )
 )
 
@@ -32,9 +31,9 @@ find hidemaru.exe "64bit edition"
 
 if %ERRORLEVEL%==0 (
 	echo "秀丸エディタ 64bit版";
-    for %%A in (%~dp0*.dll) do (
+    for /R %targetdir% %%A in (*.dll) do (
         echo %%A
-        C:\Windows\Microsoft.NET\Framework64\v4.0.30319\ngen.exe install %%A /AppBase:"%CURRENT_TMP_DIR_WITHOUT_LAST_SEPARATOR%"
+        C:\Windows\Microsoft.NET\Framework64\v4.0.30319\ngen.exe install %%~fA /AppBase:"%CURRENT_TMP_DIR_WITHOUT_LAST_SEPARATOR%"
     )
 
 )
